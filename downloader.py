4754ed3f2b20f61abb96abbb1910552d352b98eb
ev3rN1ght <nightsun1996@gmail.com>
2020-09-27 18:31:02 +0800

    parse

--- downloader.py
+++ downloader.py
@@ -4,3 +4,21 @@
 
 import requests
 import re
+import argparse
+
+def testurl(url):
+    pattern = r'http[s]?://(.*)'
+    if re.match(pattern,url):
+        print '[*] valid'
+    else:
+        print '[*] invalid url'
+
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description='Back up any webpage to the local')
+    parser.add_argument('--url',dest = 'url', help='input your url')
+    args = parser.parse_args()
+
+    url = args.url
+    testurl(url)
+
