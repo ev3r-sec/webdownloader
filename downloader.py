3bc01ffa4947d6b7f70bb7e20e2b50b4b31aa727
ev3rN1ght <nightsun1996@gmail.com>
2020-09-29 19:28:17 +0800

    some bugs

--- downloader.py
+++ downloader.py
@@ -8,13 +8,15 @@ import os
 import argparse
 from bs4 import BeautifulSoup
 
+basedir = ""
 jsbasedir = "js"
 cssbasedir = "css"
 
 def testurl(url):
     pattern = r'http[s]?://(.*)'
     if re.match(pattern,url):
-        return True
+        result = re.findall(pattern,url)[0]
+        return result
     else:
         return False
 
@@ -23,7 +25,6 @@ def proctext(html):
     soup = BeautifulSoup(html, 'lxml')
 
     script = soup.findAll("script",attrs={"src":True})
-    #  print script
     for item in script:
 
         src = item['src']
@@ -37,25 +38,32 @@ def saveotherfile(link):
     filename = link.split('/')[-1]
 
     if filename.split('.')[-1] == 'js':
-        print filename
+        content = requests.get(url=link).text
+        if not os.path.exists(jsbasedir):
+            os.makedirs(jsbasedir)
+        newname = basedir + "/" + jsbasedir + "/" + filename
+        with open(newname, "w") as f:
+            f.write(content)
+        return newname
 
 
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='Back up any webpage to the local')
-    parser.add_argument('--url',dest = 'url', help='input your url')
+    parser.add_argument('--url',dest = 'url', help='input your url', required=True)
     args = parser.parse_args()
 
     url = args.url
 
-    if testurl(url):
+    splurl = testurl(url)
+    if splurl != "No":
+        basedir = splurl
+        if not os.path.exists(basedir):
+            os.makedirs(basedir)
 
         page = requests.get(url=url)
-        #  print len(page.text)
         pagedic = proctext(page.text)
-        #  savefile(pagedic)
 
     else:
         print "[*] Invalid URL"
-
 
